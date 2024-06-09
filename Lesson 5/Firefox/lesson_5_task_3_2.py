6554dd337ef5e16511445291af24a272e240a56b
Natbolt <edusmart9696@yandex.ru>
2024-06-09 12:59:28 +0300

    hw66

---
+++ Lesson 5/Firefox/lesson_5_task_3_2.py
@@ -0,0 +1,24 @@
+from selenium import webdriver
+from time import sleep
+
+firefox = webdriver.Firefox()
+
+try:
+    count = 0
+    firefox.get("http://uitestingplayground.com/dynamicid")
+    # Кликаем на синюю кнопку
+    blue_button = firefox.find_element(
+        "xpath", '//button[text()="Button with Dynamic ID"]').click()
+   
+    # Кликаем на синюю кнопку 3 раза
+    for _ in range(3):
+        blue_button = firefox.find_element(
+            "xpath", '//button[text()="Button with Dynamic ID"]').click()
+        count = count + 1
+        sleep(2)
+        print(count)
+
+except Exception as ex:
+    print(ex)
+finally:
+    firefox.quit()
